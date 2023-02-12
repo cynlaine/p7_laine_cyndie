9100a98ce35b82e46b5a8b7ac2c76d619e6fd961
cynlaine <cyndevdev@gmail.com>
2023-02-12 11:03:34 +0100

    watch script not needed with react

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-import "./index.css";
+import "./sass/main.scss";
 import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer";
 
