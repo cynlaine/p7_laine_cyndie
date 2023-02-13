dc4f6383960f03e79f080c81b5d4c19d806b2793
cynlaine <cyndevdev@gmail.com>
2023-02-13 11:34:10 +0100

    props.children feature added for about page

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -36,6 +36,7 @@ export default function Dropdown(props) {
             ) : (
                 <div className={isOpen ? "content" : "content--hidden"}>
                     {props.content}
+                    {props.children}
                 </div>
             )}
         </div>
